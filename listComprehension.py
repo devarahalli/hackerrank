f2cd8e7ab46af76c52d433df104bd61669e938a7
xyz12dfd <pd@amc.com>
2022-03-27 15:11:19 +0530

    Added descriptive texts and if condition

--- listComprehension.py
+++ listComprehension.py
@@ -1,8 +1,8 @@
 if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
+    x = int(input('Enter the value for x = '))
+    y = int(input('Enter the value for y = '))
+    z = int(input('Enter the value for z = '))
+    n = int(input('Enter the value for n = '))
 
-    r = [(i,j,k) for i in range(x+1) for j in range(y+1) for k in range(z+1) ]
+    r = [(i,j,k) for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k !=n ]
     print (r)
